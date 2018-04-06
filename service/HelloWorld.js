93c7b4e80bc41d0452291b8f620c3797a5642225
unknown <aizuguo_@126.com>
2018-04-07 01:15:51 +0800

    fix

--- service/HelloWorld.js
+++ service/HelloWorld.js
@@ -7,16 +7,17 @@ exports.execute = function (message, config) {
     logger.info(module, 'servicing...');
     return new Promise(function (resolve, reject) {
         var text = message.text.trim().toLowerCase();
-        if (text === 'helloworld') {
+        if (text === 'helloworld') {	
             resolve('this service shows you how to write a new service');
             return;
+		}	
 
-            reject('if there is no resolve i will be executed');
-            return;
-            // if you neither resolve nor reject, it will be waiting there for return, but no impact on new requests
-        }
+		reject('if there is no return after resolve i will be executed');
+		return;
+		
+		// if you neither resolve nor reject, it will be waiting there for return, but no impact on new requests
         resolve(undefined);
     });
 }
 
-
+this.execute({text: 'helloworld'});
