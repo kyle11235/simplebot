267984c282582e31346a0a87873282108c287cf6
someone <someone@example.com>
2021-05-25 16:24:05 +0800

    c

--- service/SayHi.js
+++ service/SayHi.js
@@ -9,11 +9,15 @@ exports.execute = function (message, config) {
     return new Promise(function (resolve, reject) {
         var text = message.text.trim().toLowerCase();
         if(text === '你好' || text === 'hi' || text === 'hello'){
-            wechatUtil.getUserProfile(message.userId, config).then(function (profile) {
-                resolve('你好 ' + profile.nickname);
-            }).catch(function(error){
-                reject(error);
-            });
+            
+            resolve('hi');
+            
+            
+            // wechatUtil.getUserProfile(message.userId, config).then(function (profile) {
+            //     resolve('你好 ' + profile.nickname);
+            // }).catch(function(error){
+            //     reject(error);
+            // });
             // make sure no code after this after your async call
             return;
         }
